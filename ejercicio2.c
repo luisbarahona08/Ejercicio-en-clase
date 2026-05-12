47abe41905ba1856556e7e4682e98ec966c5204e
Luis Barahona <ldavidbara@gmail.com>
2026-05-12 11:43:58 -0500

    Impresion con puntero

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main(){
-    char vocales[] = {'a','e','i','o','u'};
-    int n =5;
-    for (int i=0; i<n; i++){
-        printf("%c ", vocales[i]);
+    char vocales[5] = {'a','e','i','o','u'};
+    char *ptr;
+    ptr = &vocales[0];
+    for (int i=0; i<5; i++){
+        printf("%c", vocales[i]);
+        printf("%c\n", *(ptr+i));
     }
 }
