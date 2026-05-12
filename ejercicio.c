623fa3287ed4a21defb7cad61ca7f8b9378c33bc
Luis Barahona <ldavidbara@gmail.com>
2026-05-12 11:16:03 -0500

    Impresion de arreglo con punteros

--- ejercicio.c
+++ ejercicio.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main(){
 int arr[]= {2,4,6,8,10};
+int *p;
+p = &arr[0];
+//impresion de arreglo con punteros
 for (int i=0; i <5; i++){
-    printf ("%d ", arr[i]);
+    printf("arr[%d] = %d", i, arr[i]);
+    p = &arr[i];
+    printf ("%d\n ", *p);
 }
-
 
 }
