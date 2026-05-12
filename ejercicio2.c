b0209fcc52474116a09ce3547f782e0031ed0c4a
Luis Barahona <ldavidbara@gmail.com>
2026-05-12 11:47:24 -0500

    cambio de valores dentro del arreglo

--- ejercicio2.c
+++ ejercicio2.c
@@ -3,6 +3,7 @@ int main(){
     char vocales[5] = {'a','e','i','o','u'};
     char *ptr;
     ptr = &vocales[0];
+    *(ptr+3) = 'x';
     for (int i=0; i<5; i++){
         printf("%c", vocales[i]);
         printf("%c\n", *(ptr+i));
